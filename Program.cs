48f53991d7c1e22da1c956f4f0920f9c8ca093ba
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 15:42:31 -0500

    naive implementation of algorithm

--- Program.cs
+++ Program.cs
@@ -15,7 +15,30 @@ namespace exam_average
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            int examScore1 = 100,
+                examScore2 = 98,
+                examScore3 = 92,
+                examScore4 = 87,
+                examScore5 = 74,
+                examTotal = 0;
+            const int NUMBER_OF_SCORES = 5;
+            double examAverage;
+            WriteLine("{0, 8}", "Scores");
+
+            // it's painful not to use an array and foreach()
+            WriteLine("{0, 8}", examScore1);
+            examTotal += examScore1;
+            WriteLine("{0, 8}", examScore2);
+            examTotal += examScore2;
+            WriteLine("{0, 8}", examScore3);
+            examTotal += examScore3;
+            WriteLine("{0, 8}", examScore4);
+            examTotal += examScore4;
+            WriteLine("{0, 8}", examScore5);
+            examTotal += examScore5;
+            WriteLine("\n{0, 8}", "Average");
+            examAverage = examTotal / NUMBER_OF_SCORES;
+            WriteLine("{0, 8:N2}", examAverage);
         }
     }
 }
