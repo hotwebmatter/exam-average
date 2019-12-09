d4af3b0f2176905f25e22e92cabe3716bf8f59ad
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 15:16:16 -0500

    Initial commit of exam-average Hello World scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exam_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
