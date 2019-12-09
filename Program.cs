607f787193ad840004224fa5f886aa9fe5c67923
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-09 15:20:39 -0500

    Add traditional ASCII banner docblock

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+ * ######################################################
+ * ##    Chapter 2, Programming Exercise #3            ##
+ * ##    Developer: Matthew Obert                      ##
+ * ##    Date Submitted: December 9 2019               ##
+ * ##    Purpose: Width & numeric format specifiers    ##
+ * ######################################################
+ */
+using System;
 
 namespace exam_average
 {
